cd3471db62e1dea4d3a75e8897cf870dea3942f4
Alexand3r <andrei.tudor27@yahoo.com>
2015-07-29 01:01:12 +0200

    It works

---
+++ Notify Tester/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notify_Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToastLib.Notify notif = new ToastLib.Notify();
+            notif.show(textBox1.Text);
+        }
+    }
+}
